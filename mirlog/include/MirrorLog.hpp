2d53c2b5acfb61abd8463545b114a6b0d03bb313
Trevor Morris <tremorris1999@gmail.com>
2022-02-22 09:54:23 -0800

    updated include hpp

--- mirlog/include/MirrorLog.hpp
+++ mirlog/include/MirrorLog.hpp
@@ -5,7 +5,7 @@
  * @file mirrorlog.hpp
  * @author Trevor Morris (https://github.com/tremorris1999)
  * @brief Simple logging library for my own personal use. Originally designed for use in my Mirror engine project.
- * @version 0.1
+ * @version 1.1
  * @date 2022-02-20
  * 
  * @copyright Copyright (c) 2022 or something
