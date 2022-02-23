a1603faf305980eac2d4c7cc9d46a2333614ed7d
Trevor Morris <tremorris1999@gmail.com>
2022-02-23 03:05:43 -0800

    removed build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/MirrorLog
-# Build directory: C:/dev/MirrorLog/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("mirlog")
-subdirs("tests")
--- build/mirlog/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/MirrorLog/mirlog
-# Build directory: C:/dev/MirrorLog/build/mirlog
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
